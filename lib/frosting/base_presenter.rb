9ba91d10b6f5e3df094d9781bdb2a8733507f347
Ben Eddy <bae@foraker.com>
2014-02-12 00:59:18 -0700

    Add base presenter specs
    Remove delegation to ActionView methods

--- lib/frosting/base_presenter.rb
+++ lib/frosting/base_presenter.rb
@@ -1,11 +1,20 @@
+require "delegate"
+
 module Frosting
   class BasePresenter < SimpleDelegator
-    delegate :link_to, :content_tag, to: :@context
-
     def initialize(resource, context = nil)
       @context = context
       @wrapped = resource
       super(@wrapped)
     end
+
+    private
+
+    def _context
+      @context
+    end
+
+    alias :view_context :_context
+    alias :h :_context
   end
 end

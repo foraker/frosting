76d34f2a214ef78e71e07de0b82e030e60c9f8c9
Jon Evans <jle@foraker.com>
2014-02-18 16:07:33 -0700

    Fix providing options from controller

--- lib/frosting/integration/rails.rb
+++ lib/frosting/integration/rails.rb
@@ -4,11 +4,11 @@ module Frosting
   class Railtie < ::Rails::Railtie
     initializer 'frosting.presentation' do
       ActiveSupport.on_load(:action_controller) do
-        define_method :present do |resource|
-          Repository.present(resource, { context: view_context })
+        define_method :present do |resource, options = {}|
+          Repository.present(resource, { context: view_context }.merge(options))
         end
 
-        define_method :present_collection do |resources, options|
+        define_method :present_collection do |resources, options = {}|
           Repository.present_collection(resources, { context: view_context }.merge(options))
         end
       end
